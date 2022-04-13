2ec774475ae6130aad4638644c87a857ca952c1d
Subrat Behera <subrat.behera@happiestminds.com>
2022-04-13 12:11:05 +0530

    second commit

--- src/main/java/com/example/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/example/jenkinsdemo/JenkinsdemoApplication.java
@@ -19,6 +19,7 @@ public class JenkinsdemoApplication {
 
 	public static void main(String[] args) {
 		logger.info("Application executed....");
+		logger.info("Second Execution....");
 		SpringApplication.run(JenkinsdemoApplication.class, args);
 	}
 
